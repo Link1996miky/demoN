55173ebcc6a173ef81cfbe9d54fb70ad02aff813
link <512819631@qq.com>
2021-04-21 16:23:15 +0800

    Scheduled Timetask

---
+++ src/main/java/com/example/demon/config/ScheduledConfig.java
@@ -0,0 +1,24 @@
+package com.example.demon.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+@Configuration
+public class ScheduledConfig implements SchedulingConfigurer {
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
+        scheduledTaskRegistrar.setScheduler(scheduledTaskExecutor());
+    }
+
+    @Bean
+    public Executor scheduledTaskExecutor(){
+        return Executors.newScheduledThreadPool(3);
+    }
+
+}
